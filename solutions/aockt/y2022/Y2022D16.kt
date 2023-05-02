b48314d444154fbd9b482f363b1064ac3e200e2b
Jadarma <dancristiancimpianu@gmail.com>
2023-05-02 13:39:01 +0300

    Add solution for Y2022D16 (part 2).

--- solutions/aockt/y2022/Y2022D16.kt
+++ solutions/aockt/y2022/Y2022D16.kt
@@ -7,137 +7,247 @@ import io.github.jadarma.aockt.core.Solution
 
 object Y2022D16 : Solution {
 
+    /**
+     * A valve in the optimised volcano.
+     *
+     * @property index Numeric identifier of this valve, calculated as it's order in the lexicographic order of valves.
+     * @property name The valve's original name, unused but useful for debugging.
+     * @property flowRate How much pressure per minute this valve can release if open.
+     * @property tunnels The cost to travel to and open a valve having the index the same as the position in the list.
+     */
     private data class Valve(
+        val index: Int,
         val name: String,
         val flowRate: Int,
-        val tunnels: Map<String, Int>,
+        val tunnels: List<Int>,
     )
 
+    /**
+     * A minimalist bitset implementation for defining a set of up to sixteen [Valve]s.
+     *
+     * @property bits The value the bits are stored in.
+     *   In the binary representation, the N-th bit starting from the least significant is set if the valve with that
+     *   index belongs in the set this mask describes.
+     */
+    @JvmInline
+    private value class ValveMask(val bits: UShort) {
+
+        /** Whether the mask contains the valve with this [index]. */
+        operator fun get(index: Int): Boolean = (1u shl index).toUShort() and bits != 0u.toUShort()
+
+        /** Syntactic sugar for checking if the mask contains this [valve]. */
+        operator fun contains(valve: Valve): Boolean = this[valve.index]
+
+        /** Returns a mask value identical to this one, but including or excluding the valve of an [index]. */
+        fun withValve(index: Int, isIncluded: Boolean = true): ValveMask {
+            if (this[index] == isIncluded) return this
+            val bitAtPosition = (1u shl index).toUShort()
+            val correctedBits = when (isIncluded) {
+                true -> bits or bitAtPosition
+                false -> bits and bitAtPosition.inv()
+            }
+            return ValveMask(correctedBits)
+        }
+    }
+
+    /**
+     * A search state for exploring the volcano.
+     *
+     * @property timeLeft How many minutes are left to search.
+     * @property lastValve What was the last opened valve.
+     * @property releasedPressure The total pressure released up until this point.
+     * @property openValves Which valves have already been opened.
+     */
     private data class State(
         val timeLeft: Int,
-        val nextValve: String?,
+        val lastValve: Int,
         val releasedPressure: Int,
-        val openValves: Set<String>,
+        val openValves: ValveMask,
     )
 
-    private class VolcanoSolver(private val valves: Map<String, Valve>) : Graph<State> {
+    /**
+     * Solver for a single player attempting to open volcano valves.
+     * @param valves The list of valves this player has access to.
+     */
+    private class VolcanoSolo(valves: List<Valve>) : Graph<State> {
 
         /** The total flow rate per minute if all of the [valves] would be open. */
-        private val maximumFlowRate = valves.values.sumOf { it.flowRate }
+        private val maximumFlowRate = valves.sumOf { it.flowRate }
 
-        /** Computes all the possible decisions to take to produce new states starting from the current one. */
+        /** Use an array for valves so that it can be indexed easily. */
+        private val valves: Array<Valve?> = Array(16) { null }
+
+        init {
+            require(valves.size <= 16) { "Your volcano is too big and would overheat the device." }
+            valves.forEach { this.valves[it.index] = it }
+        }
+
+        /**
+         * Computes all the possible decisions to take to produce new states starting from the current one.
+         * - If no time left, stop here.
+         * - If no valves can be opened (either because none left or not enough time to reach them), skip to the end.
+         * - For all valid valve choices available, calculate the next state by traveling there.
+         */
         private fun neighbors(node: State): List<State> {
+            if (node.timeLeft == 0) return emptyList()
 
-            // If all valves have been opened, just wait until time runs out.
-            if (node.openValves.size == valves.size) {
-                return listOf(
+            return valves
+                .filterNotNull()
+                .filter { it !in node.openValves }
+                .map { next -> next.index to valves[node.lastValve]!!.tunnels[next.index] }
+                .filter { it.second <= node.timeLeft }
+                .ifEmpty { listOf(0 to node.timeLeft) }
+                .map { (next, time) ->
+                    val flowRate = valves.filterNotNull().filter { it in node.openValves }.sumOf { it.flowRate }
                     State(
-                        timeLeft = 0,
-                        nextValve = null,
-                        releasedPressure = node.releasedPressure + node.timeLeft * maximumFlowRate,
-                        openValves = node.openValves,
+                        timeLeft = node.timeLeft - time,
+                        lastValve = next,
+                        releasedPressure = node.releasedPressure + flowRate * time,
+                        openValves = node.openValves.withValve(next),
                     )
-                )
-            }
-
-            val closedValves = valves.keys - node.openValves - "AA"
-
-            return closedValves
-                .mapNotNull { next ->
-                    val costToOpen = node.nextValve!!.let(valves::getValue).tunnels.getValue(next)
-                    if (costToOpen > node.timeLeft) return@mapNotNull null
-
-                    State(
-                        timeLeft = node.timeLeft - costToOpen,
-                        nextValve = next,
-                        releasedPressure = node.releasedPressure + costToOpen * node.openValves.sumOf {
-                            valves.getValue(
-                                it
-                            ).flowRate
-                        },
-                        openValves = node.openValves + next,
-                    )
-                }
-                .ifEmpty {
-                    // Not enough time to open any other valve, wait the rest of the time.
-                    State(
-                        timeLeft = 0,
-                        nextValve = null,
-                        releasedPressure = node.releasedPressure + node.timeLeft * node.openValves.sumOf {
-                            valves.getValue(
-                                it
-                            ).flowRate
-                        },
-                        openValves = node.openValves,
-                    ).let { listOf(it) }
                 }
         }
 
         /**
-         * The transition cost between two states is defined as the amount of pressure not released because of closed
-         * valves.
+         * Because we want to maximise the pressure released, the transition cost between two states is defined as the
+         * total amount of pressure not released because of closed valves.
          */
         private fun cost(from: State, to: State): Int =
-            from.openValves
-                .map(valves::getValue)
+            valves
+                .filterNotNull()
+                .filter { it in from.openValves }
                 .sumOf(Valve::flowRate)
                 .let { currentFlowRate -> maximumFlowRate - currentFlowRate }
                 .times(from.timeLeft - to.timeLeft)
 
         override fun neighboursOf(node: State) = neighbors(node).map { next -> next to cost(node, next) }
 
-        fun solve(): Int = search(
-            start = State(30, "AA", 0, emptySet()),
-            goalFunction = { it.timeLeft == 0 }
-        ).destination!!.releasedPressure
+        /**
+         * Using Djikstra's algorithm, explore the volcano and find the maximum amount of pressure that can be released.
+         * Unfortunately, due to "optimisations", the actual path to take is not returned, only the pressure value,
+         * but it can be computed if you wanted to: `solution.path()!!.path.map { it.lastValve }`, and then convert the
+         * map the valve indexes back to their names again.
+         *
+         * TODO: A possible optimisation is to find a heuristic function to speed up the search.
+         *       However, due to how the cost is calculated, initial attempts of a heuristic made it worse, because
+         *       estimation takes more than just "winging it".
+         *
+         * @param time How many minutes are available to explore.
+         */
+        fun solve(time: Int): Int {
+            val start = State(time, 0, 0, ValveMask(1u))
+            val solution = search(start) { it.timeLeft == 0 }.destination
+            checkNotNull(solution) { "A solution must exist because time moves forwards." }
+            return solution.releasedPressure
+        }
     }
 
     /**
-     * Parses the [input] and returns the valves, keyed by their name.
+     * Solver for two players attempting to open volcano valves.
+     * @param valves The list of all valves the players can split between themselves.
+     */
+    private class VolcanoTeam(private val valves: List<Valve>) {
+
+        init {
+            require(valves.size <= 16) { "Your volcano is too big and would overheat the device." }
+        }
+
+        /** Divide the volcano into a smaller one using the [mask]. */
+        private fun partition(mask: ValveMask): VolcanoSolo =
+            valves
+                .filter { it in mask }
+                .map { valve -> valve.copy(tunnels = valve.tunnels.mapIndexed { index, cost -> if (mask[index]) cost else -1 }) }
+                .let(::VolcanoSolo)
+
+        /**
+         * Explore the volcano using two players and find the maximum amount of pressure that can be released.
+         * Generates all possible partitions of the volcano, assigning a subset of valves to the first player and the
+         * rest to the second, ensuring no overlaps.
+         * Simulate all possibilities as a single player, then find the total best by adding the result of each one with
+         * the result of its complement, and returning the maximum sum.
+         * Again, because of optimisation, the actual path taken is not returned, though it could be determined in
+         * theory.
+         *
+         * @param time How many minutes are available to explore.
+         */
+        fun solve(time: Int): Int {
+            val totalPartitions = (1u shl valves.size).dec().toUShort()
+            val solutionCache: MutableMap<ValveMask, Int> = mutableMapOf()
+
+            (0u..totalPartitions.toUInt())
+                .asSequence()
+                .map { ValveMask((it shl 1).or(1u).toUShort()) }
+                .forEach { solutionCache[it] = partition(it).solve(time) }
+
+            return solutionCache.maxOf { (myValves, myPressure) ->
+                val elephantValves = ValveMask(myValves.bits.inv() and totalPartitions or 1u)
+                val elephantPressure = solutionCache.getValue(elephantValves)
+                myPressure + elephantPressure
+            }
+        }
+    }
+
+    /**
+     * Parses the [input] and returns the valves.
      * Instead of returning the actual graph of all the valves, returns an equivalent fully-connected graph that only
      * contains the valves that have a positive flow rate, with "virtual tunnels" to all other valves in the volcano,
-     * with their cost adjusted to the minimum possible value for pathfinding in the real volcano.
+     * with their cost adjusted to the minimum possible value for pathfinding in the real volcano, as well as the extra
+     * minute required to open them.
+     *
+     * TODO: For computing the optimised valve graph, Floyd-Warshall would be nicer instead of searching from every
+     *       node, but for practical purposes it doesn't matter since the graphs are small enough not to have an impact
+     *       in performance.
      */
-    private fun parseInput(input: String): VolcanoSolver {
+    private fun parseInput(input: String): List<Valve> {
         val inputRegex = Regex("""^Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)$""")
+
+        data class RawValve(val name: String, val flowRate: Int, val tunnels: Map<String, Int>)
 
         val valves = input
             .lineSequence()
             .mapNotNull(inputRegex::matchEntire)
             .map(MatchResult::destructured)
             .associate { (name, flow, tunnel) ->
-                name to Valve(
+                name to RawValve(
                     name,
                     flow.toInt(),
-                    tunnel.split(", ").toSet().associateWith { 1 }.toMap()
+                    tunnel.split(", ").toSet().associateWith { 1 }
                 )
             }
 
-        val unoptimisedVolcano = object : Graph<Valve> {
-            override fun neighboursOf(node: Valve): List<Pair<Valve, Int>> = valves
+        val unoptimisedVolcano = object : Graph<RawValve> {
+            override fun neighboursOf(node: RawValve): List<Pair<RawValve, Int>> = valves
                 .getValue(node.name)
                 .tunnels.keys
                 .map(valves::getValue)
                 .map { it to 1 }
         }
 
-        val importantValves = valves.filterValues { it.name == "AA" || it.flowRate > 0 }.map { it.value }
+        val importantValves = valves
+            .filterValues { it.name == "AA" || it.flowRate > 0 }
+            .map { it.value }
+            .sortedBy { it.name }
 
         return importantValves
             .map { valve -> valve to unoptimisedVolcano.search(valve, goalFunction = { false }) }
             .map { (valve, search) ->
                 Valve(
+                    index = importantValves.indexOf(valve),
                     name = valve.name,
                     flowRate = valve.flowRate,
-                    tunnels = importantValves
-                        .asSequence()
-                        .filterNot { it == valve || it.name == "AA" }
-                        .associate { it.name to search.pathTo(it)!!.size }
+                    tunnels = run {
+                        val tunnelArray = IntArray(16) { -1 }
+                        importantValves
+                            .asSequence()
+                            .withIndex()
+                            .filterNot { it.value == valve || it.value.name == "AA" }
+                            .forEach { tunnelArray[it.index] = search.pathTo(it.value)!!.size }
+                        tunnelArray.toList()
+                    }
                 )
             }
-            .associateBy { it.name }
-            .let(::VolcanoSolver)
     }
 
-    override fun partOne(input: String) = parseInput(input).solve()
+    override fun partOne(input: String) = parseInput(input).let(::VolcanoSolo).solve(30)
+    override fun partTwo(input: String) = parseInput(input).let(::VolcanoTeam).solve(26)
 }
--- tests/aockt/y2022/Y2022D16Test.kt
+++ tests/aockt/y2022/Y2022D16Test.kt
@@ -2,7 +2,12 @@ package aockt.y2022
 
 import io.github.jadarma.aockt.test.AdventDay
 import io.github.jadarma.aockt.test.AdventSpec
+import kotlin.time.Duration.Companion.minutes
 
+/**
+ * Extra test inputs for this day taken from `/u/i_have_no_biscuits`'s post here:
+ * [reddit post](https://reddit.com/r/adventofcode/comments/znklnh/2022_day_16_some_extra_test_cases_for_day_16/)
+ */
 @AdventDay(2022, 16, "Proboscidea Volcanium")
 class Y2022D16Test : AdventSpec<Y2022D16>({
 
@@ -19,5 +24,122 @@ class Y2022D16Test : AdventSpec<Y2022D16>({
         Valve JJ has flow rate=21; tunnel leads to valve II
     """.trimIndent()
 
-    partOne { exampleInput shouldOutput 1651 }
+    val aLinearLine = """
+        Valve LA has flow rate=22; tunnels lead to valves KA, MA
+        Valve MA has flow rate=24; tunnels lead to valves LA, NA
+        Valve NA has flow rate=26; tunnels lead to valves MA, OA
+        Valve OA has flow rate=28; tunnels lead to valves NA, PA
+        Valve PA has flow rate=30; tunnels lead to valves OA
+        Valve AA has flow rate=0; tunnels lead to valves BA
+        Valve BA has flow rate=2; tunnels lead to valves AA, CA
+        Valve CA has flow rate=4; tunnels lead to valves BA, DA
+        Valve DA has flow rate=6; tunnels lead to valves CA, EA
+        Valve EA has flow rate=8; tunnels lead to valves DA, FA
+        Valve FA has flow rate=10; tunnels lead to valves EA, GA
+        Valve GA has flow rate=12; tunnels lead to valves FA, HA
+        Valve HA has flow rate=14; tunnels lead to valves GA, IA
+        Valve IA has flow rate=16; tunnels lead to valves HA, JA
+        Valve JA has flow rate=18; tunnels lead to valves IA, KA
+        Valve KA has flow rate=20; tunnels lead to valves JA, LA
+    """.trimIndent()
+
+    val aQuadraticLine = """
+        Valve AA has flow rate=0; tunnels lead to valves BA
+        Valve BA has flow rate=1; tunnels lead to valves AA, CA
+        Valve CA has flow rate=4; tunnels lead to valves BA, DA
+        Valve DA has flow rate=9; tunnels lead to valves CA, EA
+        Valve EA has flow rate=16; tunnels lead to valves DA, FA
+        Valve FA has flow rate=25; tunnels lead to valves EA, GA
+        Valve GA has flow rate=36; tunnels lead to valves FA, HA
+        Valve HA has flow rate=49; tunnels lead to valves GA, IA
+        Valve IA has flow rate=64; tunnels lead to valves HA, JA
+        Valve JA has flow rate=81; tunnels lead to valves IA, KA
+        Valve KA has flow rate=100; tunnels lead to valves JA, LA
+        Valve LA has flow rate=121; tunnels lead to valves KA, MA
+        Valve MA has flow rate=144; tunnels lead to valves LA, NA
+        Valve NA has flow rate=169; tunnels lead to valves MA, OA
+        Valve OA has flow rate=196; tunnels lead to valves NA, PA
+        Valve PA has flow rate=225; tunnels lead to valves OA
+    """.trimIndent()
+
+    val aCircle = """
+        Valve BA has flow rate=2; tunnels lead to valves AA, CA
+        Valve CA has flow rate=10; tunnels lead to valves BA, DA
+        Valve DA has flow rate=2; tunnels lead to valves CA, EA
+        Valve EA has flow rate=10; tunnels lead to valves DA, FA
+        Valve FA has flow rate=2; tunnels lead to valves EA, GA
+        Valve GA has flow rate=10; tunnels lead to valves FA, HA
+        Valve HA has flow rate=2; tunnels lead to valves GA, IA
+        Valve IA has flow rate=10; tunnels lead to valves HA, JA
+        Valve JA has flow rate=2; tunnels lead to valves IA, KA
+        Valve KA has flow rate=10; tunnels lead to valves JA, LA
+        Valve LA has flow rate=2; tunnels lead to valves KA, MA
+        Valve MA has flow rate=10; tunnels lead to valves LA, NA
+        Valve NA has flow rate=2; tunnels lead to valves MA, OA
+        Valve OA has flow rate=10; tunnels lead to valves NA, PA
+        Valve PA has flow rate=2; tunnels lead to valves OA, AA
+        Valve AA has flow rate=0; tunnels lead to valves BA, PA
+    """.trimIndent()
+
+    val clusters = """
+        Valve AK has flow rate=100; tunnels lead to valves AJ, AW, AX, AY, AZ
+        Valve AW has flow rate=10; tunnels lead to valves AK
+        Valve AX has flow rate=10; tunnels lead to valves AK
+        Valve AY has flow rate=10; tunnels lead to valves AK
+        Valve AZ has flow rate=10; tunnels lead to valves AK
+        Valve BB has flow rate=0; tunnels lead to valves AA, BC
+        Valve BC has flow rate=0; tunnels lead to valves BB, BD
+        Valve BD has flow rate=0; tunnels lead to valves BC, BE
+        Valve BE has flow rate=0; tunnels lead to valves BD, BF
+        Valve BF has flow rate=0; tunnels lead to valves BE, BG
+        Valve BG has flow rate=0; tunnels lead to valves BF, BH
+        Valve BH has flow rate=0; tunnels lead to valves BG, BI
+        Valve BI has flow rate=0; tunnels lead to valves BH, BJ
+        Valve BJ has flow rate=0; tunnels lead to valves BI, BK
+        Valve BK has flow rate=100; tunnels lead to valves BJ, BW, BX, BY, BZ
+        Valve BW has flow rate=10; tunnels lead to valves BK
+        Valve BX has flow rate=10; tunnels lead to valves BK
+        Valve BY has flow rate=10; tunnels lead to valves BK
+        Valve BZ has flow rate=10; tunnels lead to valves BK
+        Valve CB has flow rate=0; tunnels lead to valves AA, CC
+        Valve CC has flow rate=0; tunnels lead to valves CB, CD
+        Valve CD has flow rate=0; tunnels lead to valves CC, CE
+        Valve CE has flow rate=0; tunnels lead to valves CD, CF
+        Valve CF has flow rate=0; tunnels lead to valves CE, CG
+        Valve CG has flow rate=0; tunnels lead to valves CF, CH
+        Valve CH has flow rate=0; tunnels lead to valves CG, CI
+        Valve CI has flow rate=0; tunnels lead to valves CH, CJ
+        Valve CJ has flow rate=0; tunnels lead to valves CI, CK
+        Valve CK has flow rate=100; tunnels lead to valves CJ, CW, CX, CY, CZ
+        Valve CW has flow rate=10; tunnels lead to valves CK
+        Valve CX has flow rate=10; tunnels lead to valves CK
+        Valve CY has flow rate=10; tunnels lead to valves CK
+        Valve CZ has flow rate=10; tunnels lead to valves CK
+        Valve AA has flow rate=0; tunnels lead to valves AB, BB, CB
+        Valve AB has flow rate=0; tunnels lead to valves AA, AC
+        Valve AC has flow rate=0; tunnels lead to valves AB, AD
+        Valve AD has flow rate=0; tunnels lead to valves AC, AE
+        Valve AE has flow rate=0; tunnels lead to valves AD, AF
+        Valve AF has flow rate=0; tunnels lead to valves AE, AG
+        Valve AG has flow rate=0; tunnels lead to valves AF, AH
+        Valve AH has flow rate=0; tunnels lead to valves AG, AI
+        Valve AI has flow rate=0; tunnels lead to valves AH, AJ
+        Valve AJ has flow rate=0; tunnels lead to valves AI, AK
+    """.trimIndent()
+
+    partOne {
+        exampleInput shouldOutput 1651
+        aLinearLine shouldOutput 2640
+        aQuadraticLine shouldOutput 13468
+        aCircle shouldOutput 1288
+        clusters shouldOutput 2400
+    }
+
+    partTwo(expensive = true, efficiencyBenchmark = 1.minutes) {
+        exampleInput shouldOutput 1707
+        aLinearLine shouldOutput 2670
+        aQuadraticLine shouldOutput 12887
+        aCircle shouldOutput 1484
+        clusters shouldOutput 3680
+    }
 })
