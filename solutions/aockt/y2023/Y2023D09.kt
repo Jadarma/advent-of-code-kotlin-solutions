9fb82a2c74787946fc9c7cda98e85d52cd821f0f
Jadarma <dancristiancimpianu@gmail.com>
2023-12-09 15:44:21 +0200

    Refactor Y2023D09.
    Part two is same as part one but with a reversed input.

--- solutions/aockt/y2023/Y2023D09.kt
+++ solutions/aockt/y2023/Y2023D09.kt
@@ -6,21 +6,18 @@ import io.github.jadarma.aockt.core.Solution
 object Y2023D09 : Solution {
 
     /** A history report from an OASIS Scanner, with the [values] of the sensor readings. */
-    private class History(val values: List<Int>) : List<Int> by values {
+    private data class History(val values: List<Int>) : List<Int> by values {
 
-        private fun diffSequence() = generateSequence(values) { it.windowed(2) { (l, r) -> r - l } }
+        /** Predict the next value based on the existing list of [values]. */
+        private fun prediction(values: List<Int>) = generateSequence(values) { it.windowed(2) { (l, r) -> r - l } }
             .takeWhile { numbers -> numbers.any { it != 0 } }
+            .sumOf { it.last() }
 
         /** Predict the next value of this report based on previously gathered [values]. */
-        val predict: Int by lazy { diffSequence().sumOf { it.last() } }
+        val predictNext: Int by lazy { prediction(values) }
 
         /** Predict the last value before this report started gathering the [values]. */
-        val extrapolate: Int by lazy {
-            diffSequence()
-                .map { it.first() }
-                .toList()
-                .foldRight(0, Int::minus)
-        }
+        val predictPrevious: Int by lazy { prediction(values.reversed()) }
     }
 
     /** Parse the [input] and return the Oasis Report histories. */
@@ -32,6 +29,6 @@ object Y2023D09 : Solution {
             .toList()
     }
 
-    override fun partOne(input: String) = parseInput(input).sumOf(History::predict)
-    override fun partTwo(input: String) = parseInput(input).sumOf(History::extrapolate)
+    override fun partOne(input: String) = parseInput(input).sumOf(History::predictNext)
+    override fun partTwo(input: String) = parseInput(input).sumOf(History::predictPrevious)
 }
