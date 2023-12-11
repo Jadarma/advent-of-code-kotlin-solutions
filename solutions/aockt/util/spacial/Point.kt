072a988a16ebe705a1b2fb0ddec4df320c4f0307
Jadarma <dancristiancimpianu@gmail.com>
2023-12-11 17:55:23 +0200

    Improve documentation of Point.

--- solutions/aockt/util/spacial/Point.kt
+++ solutions/aockt/util/spacial/Point.kt
@@ -2,11 +2,21 @@ package aockt.util.spacial
 
 import kotlin.math.absoluteValue
 
-/** Represents a discrete point in 2D space. */
+/**
+ * Represents a discrete point in 2D space.
+ *
+ * **NOTE:** _The origin point is by convention on the bottom-left-hand side._
+ *
+ * @property x The horizontal component, positive values on the right of the origin.
+ * @property y The vertical component, positive values on above the origin.
+ */
 data class Point(val x: Long, val y: Long) {
 
     /** Alternate constructor that converts from integer coordinates, for convenience. */
     constructor(x: Int, y: Int) : this(x.toLong(), y.toLong())
+
+    /** Encoded as a human and debugger friendly way. */
+    override fun toString(): String = "($x, $y)"
 }
 
 /**
