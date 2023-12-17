10b6fca3bd0a7861649ecc6a939cb834bf363948
Jadarma <dancristiancimpianu@gmail.com>
2023-12-18 00:26:54 +0200

    Add rotations to direction.

--- solutions/aockt/util/spacial/Direction.kt
+++ solutions/aockt/util/spacial/Direction.kt
@@ -29,15 +29,33 @@ sealed interface Direction {
 
 /** Returns the direction opposite this one. */
 val Direction.opposite: Direction
-    get() = when(this) {
-    Left -> Right
-    Right -> Left
-    Down -> Up
-    Up -> Down
-}
+    get() = when (this) {
+        Left -> Right
+        Right -> Left
+        Down -> Up
+        Up -> Down
+    }
+
+/** Returns the direction a quarter clockwise turn away. */
+val Direction.turnedClockwise: Direction
+    get() = when (this) {
+        Left -> Up
+        Right -> Down
+        Down -> Left
+        Up -> Right
+    }
+
+/** Returns the direction a quarter counter-clockwise turn away. */
+val Direction.turnedCounterClockwise: Direction
+    get() = when (this) {
+        Left -> Down
+        Right -> Up
+        Down -> Right
+        Up -> Left
+    }
 
 /** Returns the point one unit distance away from this one in a given [direction]. */
-fun Point.move(direction: Direction): Point = when(direction) {
+fun Point.move(direction: Direction): Point = when (direction) {
     Left -> Point(x - 1, y)
     Right -> Point(x + 1, y)
     Down -> Point(x, y - 1)
