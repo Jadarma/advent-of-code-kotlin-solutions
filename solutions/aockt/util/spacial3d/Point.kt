342a0cda0243d961dde1b5df065ec0d27e272ea9
Jadarma <dancristiancimpianu@gmail.com>
2025-12-08 13:20:22 +0200

    Add util for Euclidean distance between 3D points.

--- solutions/aockt/util/spacial3d/Point.kt
+++ solutions/aockt/util/spacial3d/Point.kt
@@ -1,5 +1,8 @@
 package aockt.util.spacial3d
 
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 /**
  * Represents a discrete point in 3D space.
  *
@@ -19,4 +22,13 @@ data class Point3D(val x: Long, val y: Long, val z: Long) {
         /** The center of the cartesian grid. */
         val Origin: Point3D = Point3D(0L, 0L, 0L)
     }
+}
+
+/** Calculates the Euclidean distance between this and the [other] point. */
+infix fun Point3D.distanceTo(other: Point3D): Long {
+    var d = 0.0
+    d += (this.x - other.x).toDouble().pow(2.0)
+    d += (this.y - other.y).toDouble().pow(2.0)
+    d += (this.z - other.z).toDouble().pow(2.0)
+    return sqrt(d).toLong()
 }
